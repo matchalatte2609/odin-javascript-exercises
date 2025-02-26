f72f1c28d63fd3b8a59547c464df23f3bf5e7cad
matchalatte2609 <shaaki.2412@gmail.com>
2025-02-25 23:38:23 -0500

    finish up to exercise 10

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, cur) => acc + cur);
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	a.reduce((acc, cur) => acc * cur);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleanStr = str.toLowerCase();
 
+  let left = 0;
+  let right = cleanStr.length - 1;
+
+  while (left < right)  {
+    if (cleanStr[left] !== cleanStr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 };
 
 // Do not edit below this line
