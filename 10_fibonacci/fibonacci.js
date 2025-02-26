6a013f951e06d87baaec5251dab189d50b571566
matchalatte2609 <shaaki.2412@gmail.com>
2025-02-25 23:37:40 -0500

    finish up to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n <= 0) return 0;
+  if (n === 1) return 1;
 
+  let a = 0;
+  let b = 1;
+  for (i=2; i <= n; i++){
+    const temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 };
 
 // Do not edit below this line
